c05fac8cc9d32715a4ad270280f30b7f4622264e
DESKTOP-H6JFRBD\jwkim <jwkim8936@naver.com>
2021-10-04 22:50:59 +0900

    Initial commit

---
+++ src/main/java/com/example/springboottest1004/SpringBootTest1004Application.java
@@ -0,0 +1,13 @@
+package com.example.springboottest1004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest1004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTest1004Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottest1004/SpringBootTest1004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottest1004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest1004ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
